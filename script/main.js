bb888ca1bef4316e2914db97969f0f36eebb7874
mukhammadyn <nurmuhammadmahmudov421@gmail.com>
2021-07-01 21:39:30 +0500

    Add javascript

--- script/main.js
+++ script/main.js
@@ -6,3 +6,18 @@ if (elHeaderBtn) {
     elHeaderNav.classList.toggle('site-header_open');
   })
 }
+
+
+var elsOrderBtn = document.querySelectorAll('.js-order-btn');
+
+var elsOrderCard = document.querySelectorAll('.order-section__form-cards');
+
+elsOrderBtn.forEach(function (button) {
+  button.addEventListener('click', function () {
+    elsOrderCard.forEach(function (removeActive) {
+      removeActive.classList.remove('order-section__form-cards--open');
+    });
+
+    button.closest('.order-section__form-cards').classList.add('order-section__form-cards--open');
+  });
+});
